b8449b9325645922175a9150cc59de41158323c2
abdullah <abdullahalhomsi520@gmail.com>
2023-07-10 02:42:38 +0300

    add settings update

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -17,106 +17,27 @@ use Yajra\DataTables\DataTables;
 
 class SettingController extends Controller
 {
-    public function about(request $request)
+    public function index()
     {
-        $abouts = About::first();
-        if ($request->ajax()) {
-            $about = About::select('*');
-            return Datatables::of($about)
-                ->addColumn('action', function ($about) {
-                    return '
-                            <button type="button" data-id="' . $about->id . '" class="btn btn-pill btn-info-light editBtn"><i class="fa fa-edit"></i></button>';
-                })
-                ->escapeColumns([])
-                ->make(true);
-        } else {
-
-            return view('Admin.setting.about.index', compact('abouts'));
-        }
-    } //end of about
-
-    public function updateabout(StoreAbout $request, $id)
-    {
-        $inputs = $request->except('id');
-
-        $about = About::findOrFail($id);
-
-        if ($about->update($inputs))
-            return response()->json(['status' => 200]);
-        else
-            return response()->json(['status' => 405]);
-    } //end of updateabout
-
-    public function terms(request $request)
-    {
-        $terms = Terms::first();
-        if ($request->ajax()) {
-            $terms = Terms::select('*');
-            return Datatables::of($terms)
-                ->addColumn('action', function ($terms) {
-                    return '
-                            <button type="button" data-id="' . $terms->id . '" class="btn btn-pill btn-info-light editBtn"><i class="fa fa-edit"></i></button>';
-                })
-                ->escapeColumns([])
-                ->make(true);
-        } else {
-            return view('Admin.setting.terms.index', compact('terms'));
-        }
-    } //end of about
-
-    public function updateterms(StoreTerms $request, $id)
-    {
-        $inputs = $request->except('id');
-
-        $terms = Terms::findOrFail($id);
-
-        if ($terms->update($inputs))
-            return response()->json(['status' => 200]);
-        else
-            return response()->json(['status' => 405]);
+        $settings = Setting::first();
+        return view('admin.settings.index', compact('settings'));
     }
 
-    public function privacy(request $request)
+    public function update(StoreSetting $request)
     {
-        $privacy = Privacy::first();
-        if ($request->ajax()) {
-            $privacy = Privacy::select('*');
-            return Datatables::of($privacy)
-                ->addColumn('action', function ($privacy) {
-                    return '
-                            <button type="button" data-id="' . $privacy->id . '" class="btn btn-pill btn-info-light editBtn"><i class="fa fa-edit"></i></button>';
-                })
-                ->escapeColumns([])
-                ->make(true);
-        } else {
-            return view('Admin.setting.privacy.index',compact('privacy'));
+        $settings = Setting::findOrFail($request->id);
+
+        $inputs = $request->all();
+
+        if ($request->has('logo'))
+        {
+            if (file_exists(public_path('assets/uploads/admins/images/') .$settings->logo)) {
+                unlink(('assets/uploads/admins/images/') .$settings->logo);
+            }
+            $inputs['logo'] =  $request->logo != null ? $this->saveImage($request->logo, 'assets/uploads/admins/images' , 'photo') : $inputs['logo'];
         }
-    } //end of about
 
-    public function updateprivacy(StorePrivacy $request, $id)
-    {
-        $inputs = $request->except('id');
-
-        $privacy = Privacy::findOrFail($id);
-
-        if ($privacy->update($inputs))
-            return response()->json(['status' => 200]);
-        else
-            return response()->json(['status' => 405]);
-    }
-
-    public function edit(Setting $setting)
-    {
-        return view('Admin.setting.parts.edit', compact('setting'));
-    }
-
-    public function update(StoreSetting $request, $id)
-    {
-        $inputs = $request->except('id');
-
-        $setting = Setting::findOrFail($id);
-
-        if ($setting->update($inputs))
+        if ($settings->update($inputs))
             return response()->json(['status' => 200]);
         else
             return response()->json(['status' => 405]);
---
+++ resources/views/Admin/settings/index.blade.php
@@ -0,0 +1,115 @@
+@extends('admin/layouts/master')
+
+@section('title')
+    الاعدادات
+@endsection
+@section('page_name')
+    الاعدادات
+@endsection
+@section('content')
+    <form method="POST" id="updateForm" class="updateForm" enctype="multipart/form-data"
+        action="{{ route('settings.update', $settings->id) }}">
+        @csrf
+        @method('PUT')
+        <input type="hidden" name="id" value="{{ $settings->id }}">
+        <div class="row mt-4">
+            <div class="col-12">
+                <div class="card" style="padding: 13px">
+                    <div class="card-header">
+                        <h3 class="card-title">قائمة الاعدادات </h3>
+                    </div>
+                    <!-- Start Row -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="name" class="form-control-label">لوجو</label>
+                                <input type="file" class="dropify" name="logo"
+                                    data-default-file="{{ asset($settings->logo) }}"
+                                    accept="image/png,image/webp , image/gif, image/jpeg,image/jpg" />
+                                <span class="form-text text-danger text-center">مسموح فقط بالصيغ التالية : png, gif, jpeg,
+                                    jpg,webp</span>
+                            </div>
+                        </div>
+                    </div>
+                    <h1 class="card-title">قائمة الاعدادات العامة : </h1>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="facebook_link">العنوان :</label>
+                                <input type="text" name="title" value="{{ $settings->title }}" class="form-control" />
+                            </div>
+                        </div>
+                    </div>
+                    <h1 class="card-title" style="font: bold">قائمة السوشيال ميديا : </h1>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="facebook_link">فيسبوك :</label>
+                                <input type="url" name="facebook" value="{{ $settings->facebook }}"
+                                    class="form-control" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="">يوتيوب :</label>
+                                <input type="url" name="youtube" value="{{ $settings->youtube }}"
+                                    class="form-control" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="">تويتر :</label>
+                                <input type="url" name="twitter" value="{{ $settings->twitter }}"
+                                    class="form-control" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="">انستجرام :</label>
+                                <input type="url" name="instagram" value="{{ $settings->instagram }}"
+                                    class="form-control" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="">لينكد ان :</label>
+                                <input type="url" name="linkedin" value="{{ $settings->linkedin }}"
+                                    class="form-control" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="">واتساب :</label>
+                                <input type="url" name="whatsapp" value="{{ $settings->whatsapp }}"
+                                    class="form-control" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="desc_ar">شروط :</label>
+                                <textarea name="terms" rows="8" class="form-control">{{ $settings->terms }}</textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="desc_en">خصوصية :</label>
+                                <textarea name="privacy" rows="8" class="form-control">{{ $settings->privacy }}</textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary" id="updateButton">تحديث</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    @include('admin/layouts/myAjaxHelper')
+@endsection
+@section('ajaxCalls')
+    <script>
+        editScript();
+    </script>
+@endsection
